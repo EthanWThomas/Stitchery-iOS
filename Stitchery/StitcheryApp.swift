30947923232ed5ce1177e015e1b03cbb63b075fa
EthanWThomas <111320646+EthanWThomas@users.noreply.github.com>
2024-10-31 22:30:43 -0600

    Initial Commit

---
+++ Stitchery/StitcheryApp.swift
@@ -0,0 +1,17 @@
+//
+//  StitcheryApp.swift
+//  Stitchery
+//
+//  Created by Ethan Thomas on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct StitcheryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
